99df078fa9fce13584169b3acba17bde684d57cd
Ryan May <rmay31@gmail.com>
2014-09-03 22:49:15 -0600

    Add more figures and start organizing.

--- Defense/figures/gen_model_tex.py
+++ Defense/figures/gen_model_tex.py
@@ -10,22 +10,18 @@ figs_v = ['Attenuation_V', 'Attenuation_Difference_V', 'Specific_Attenuation_V_s
 
 template = r'''\begin{{frame}}
     \begin{{center}}
-        \includegraphics<1>[scale=0.45]{{figures/{0}_{1}_{2}}}
-        \includegraphics<2>[scale=0.45]{{figures/{0}_Control_{2}}}
+        \includegraphics<1>[scale=0.7]{{figures/{0}_{1}_{2}}}
+        \includegraphics<2>[scale=0.7]{{figures/{0}_Control_{2}}}
     \end{{center}}
 \end{{frame}}
 '''
 
 with open('gentex.tex', 'w') as outp:
     for exp in exps:
-        #outp.write('\section{{{0}}}\n\n'.format(exp))
         for band in bands:
-            #outp.write('\FloatBarrier\n\subsection{{{0} band}}\n\n'.format(band))
             for f in figs:
                 outp.write(template.format(band, exp, f) + '\n')
     for exp in exps:
-        #outp.write('\section{{{0}}}\n\n'.format(exp))
         for band in bands:
-            #outp.write('\FloatBarrier\n\subsection{{{0} band}}\n\n'.format(band))
             for f in figs_v:
                 outp.write(template.format(band, exp, f) + '\n')
--- Defense/figures/gen_spatial_tex.py
+++ Defense/figures/gen_spatial_tex.py
@@ -2,23 +2,26 @@ bands = ['C', 'X']
 exps = ['Control', 'Sidelobe', 'Beamwidth', 'Radial Width', 'Range Resolution', 'Combined']
 
 figs = ['Attenuation_H', 'Attenuation_Difference_H',
-        'Attenuation_V', 'Attenuation_Difference_V',
-        'Specific_Attenuation_H_scatter', 'Specific_Attenuation_V_scatter',
+        'Specific_Attenuation_H_scatter',
         'Differential_Attenuation', 'Differential_Attenuation_Difference',
         'Specific_Differential_Attenuation_scatter']
 
-template = r'''\begin{{figure}}
-    \centering
-    \includegraphics[scale=0.95]{{figures/spatial/{0}_{1}_{2}}}
-    \caption{{{0} {1}}}
-    \label{{fig:{0}_sp_{1}_{2}}}
-\end{{figure}}
+figs_v = ['Attenuation_V', 'Attenuation_Difference_V', 'Specific_Attenuation_V_scatter']
+
+template = r'''\begin{{frame}}
+    \begin{{center}}
+        \includegraphics<1>[scale=0.7]{{figures/spatial/{0}_{1}_{2}}}
+        \includegraphics<2>[scale=0.7]{{figures/spatial/{0}_Control_{2}}}
+    \end{{center}}
+\end{{frame}}
 '''
 
 with open('gentex.tex', 'w') as outp:
     for exp in exps:
-        outp.write('\section{{{0}}}\n\n'.format(exp))
         for band in bands:
-            outp.write('\FloatBarrier\n\subsection{{{0} band}}\n\n'.format(band))
             for f in figs:
+                outp.write(template.format(band, exp.replace(' ', ''), f) + '\n')
+    for exp in exps:
+        for band in bands:
+            for f in figs_v:
                 outp.write(template.format(band, exp.replace(' ', ''), f) + '\n')
